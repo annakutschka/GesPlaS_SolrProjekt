ecc7a262cd561d3779368808de9564911d6018e8
Isabella Margarita Sperr <isperr.mmt-b2016@fh-salzburg.ac.at>
2018-06-11 11:45:40 +0200

    fix: try to fix json

--- index.js
+++ index.js
@@ -1,96 +1,62 @@
 const fs = require('fs')
 const readline = require('readline')
 const stream = require('stream')
-const request = require('sync-request')
+//const request = require('sync-request')
 const testFolder = 'JSON_Rohdaten';
+const read = require('read-file');
 
 
 fs.readdir(testFolder, (err, files) => { //preprocessing the files
 
   files.forEach(file => { // loops over every file in resources
     let plaintext = read.sync(`${testFolder}/${file}`, 'utf8'); //reads the input of the txt-file
-    const outstream = new stream;
+    //const outstream = new stream;
 
     const input = JSON.parse(plaintext);
     const outJson = {}
 
-    outstream = createOutputFile(input, outJson);
+    createOutputFile(input, outJson);
 
   });
 
 })
 
+let documents = []
 
 function createOutputFile(inputfile, outJson){
 
-  const outfile = {}
-
-  for(int i = 0; i < inputfile.length; i++){
+  //const outfile = {}
+  //console.log(inputfile["features"][0]["attributes"])
+  for(let i = 0; i < inputfile["features"].length; i++){
+    outJson = {}
 
     const field_value = inputfile["features"][i]["attributes"];
-    outJson["diensstelle_text_de"] = field_value.DienstSt
-    outJson["art_text_de"] = field_value.Art
-    outJson["ort_text_de"] = field_value.Ort
-    outJson["str"] = field_value.Strasse
-    outJson["link_txt_sort"] = field_value.Hnr
-    outJson["popu_f"] = field_value.Bezirk
-    outJson["popu_f"] = field_value.Typ
+    //console.log(field_value)
+    //console.log("_________")
+    outJson["ka_i"] = field_value.KA_NUMMER == undefined ? null : field_value.KA_NUMMER
+    outJson["bezeichn_text_de"] = field_value.BEZEICHN
+    outJson["art_text_de"] = field_value.ART
+    outJson["ort_text_de"] = field_value.ORT
+    let strasse = [field_value.STRASSE, field_value.HAUSNUMMER]
+    //strasse.push(field_value.STRASSE)
+    //strasse.push(field_value.HAUSNUMMER)
+    outJson["str_txt_sort"] = strasse
 
-    outfile += outJson;
+
+    //jsonArr.push(outJson)
+    accumData(outJson)
+    //outfile += outJson;
 
   }
 
-  return outfile
+
+
+  //return outfile
 }
 
-
-
-
-//////////////////////////////////////////
-
-let documents = []
-
-// read file through fs readstream
-const instream = fs.createReadStream('RotesKreuz.json') // simplewiki file has to be named 'simplewiki.json'
-const outstream = new stream
-const rl = readline.createInterface(instream, outstream)
-
-// do this for every line in the stream
-rl.on('line', function(line) {
-
-    // extract only the relevant properties of the input JSON file
-    const lineJson = JSON.parse(line);
-    const outJson = {}
-
-    // write relevant properties to output JSON file
-    outJson["aux_txt_sort"] = lineJson.auxiliary_text
-    outJson["title_txt_en"] = lineJson.title
-    outJson["text_txt_en"] = lineJson.text
-    outJson["heading_txt_sort"] = lineJson.heading
-    outJson["link_txt_sort"] = lineJson.outgoing_link
-    outJson["popu_f"] = lineJson.popularity_score
-    // opening_text is optional
-    if(lineJson.opening_text) outJson["opening_txt_en"] = lineJson.opening_text
-    else outJson["opening_txt_en"] = ""
-    // coordinates is optional
-    if(lineJson.coordinates) outJson["location"] = lineJson.coordinates
-    else outJson["location"] = []
-
-    //accumulate JSON objects before they are sent
-    accumData(outJson)
-})
-
-// send rest of Data when filestream is over
-// otherwise the last <10k Objects are not sent
-rl.on('close', function() {
-    sendData(documents)
-    documents = []
-});
-
-// accumulates JSON objects in array until 10k before they are sent
 function accumData(postData) {
     documents.push(postData)
-    if(documents.length == 10000){
+    if(documents.length == 10){
         // send array of JSON objects to solr server
         console.log('sending')
         sendData(documents)
@@ -115,3 +81,10 @@ function sendData(postData){
       console.log('sent')
     }
 }
+
+// send rest of Data when filestream is over
+// otherwise the last <10k Objects are not sent
+/*rl.on('close', function() {
+    sendData(documents)
+    documents = []
+});*/
