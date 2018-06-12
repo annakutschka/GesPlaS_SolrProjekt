1cff7a39521c7254c27d01c325b9228ebfa839f4
Melanie Reisenbichler <mreisenbichler.mmt-b2016@fh-salzburg.ac.at>
2018-06-12 14:19:44 +0200

    fix: try fix sending data

--- Datenverarbeitung/index.js
+++ Datenverarbeitung/index.js
@@ -5,7 +5,7 @@ const request = require('sync-request')
 const testFolder = 'JSON_Rohdaten';
 const read = require('read-file');
 
-/*
+
 fs.readdir(testFolder, (err, files) => { //preprocessing the files
 
   files.forEach(file => { // loops over every file in resources
@@ -19,17 +19,17 @@ fs.readdir(testFolder, (err, files) => { //preprocessing the files
 
   });
 
-})*/
+})
 
 let documents = []
 
 // read file through fs readstream
-const instream = fs.createReadStream('JSON_Rohdaten/Ambukrankpotheken.json') // simplewiki file has to be named 'simplewiki.json'
+/*const instream = fs.createReadStream('JSON_Rohdaten/Ambukrankpotheken.json') // simplewiki file has to be named 'simplewiki.json'
 const outstream = new stream
 let outJson = {}
 const rl = readline.createInterface(instream, outstream)
-
-rl.on('inputfile', function(inputfile, outJson) {
+*/
+function createOutputFile(inputfile, outJson) {
   //console.log(inputfile["features"][0]["attributes"])
   for(let i = 0; i < inputfile["features"].length; i++){
     outJson = {}
@@ -38,6 +38,7 @@ rl.on('inputfile', function(inputfile, outJson) {
     //console.log(field_value)
     outJson["ka_i"] = field_value.KA_NUMMER == undefined ? null : field_value.KA_NUMMER
     outJson["bezeichn_text_de"] = field_value.BEZEICHN
+    //TODO: if bei Typ statt Art
     outJson["art_text_de"] = field_value.ART
     outJson["ort_text_de"] = field_value.ORT
     let strasse = [field_value.STRASSE, field_value.HAUSNUMMER]
@@ -45,18 +46,18 @@ rl.on('inputfile', function(inputfile, outJson) {
 
     accumData(outJson)
   }
-})
+}
 
 // send rest of Data when filestream is over
 // otherwise the last <10k Objects are not sent
-rl.on('close', function() {
+/*rl.on('close', function() {
     sendData(documents)
     documents = []
-});
+});*/
 
 function accumData(postData) {
     documents.push(postData)
-    if(documents.length == 10000){
+    if(documents.length == 10){
         // send array of JSON objects to solr server
         console.log('sending')
         sendData(documents)
@@ -66,6 +67,7 @@ function accumData(postData) {
 
 // sends data to solr server
 function sendData(postData){
+
     var clientServerOptions = {
         body: JSON.stringify(postData),
         method: 'POST',
