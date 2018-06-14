506702f91090b1c63a9c1db22d4e5ce121943e98
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-06-14 11:45:58 +0200

    Added processing logic

--- Suchmaschine/public/search.js
+++ Suchmaschine/public/search.js
@@ -1,42 +1,99 @@
 //Components
 var query = $('#search-input');
-var plz = $('#search-plz');
-var doctors = $('#filter-doctor');
 var pharmacy = $('#filter-pharmacy');
 var hospital = $('#filter-hospital');
-var redCross = $('#filter-red-cross');
 var submit = $('#search-button');
 
-var url = '/search?';
-var parameters = [];
+var searchError = $('.search-error');
 
+var results;
+var snippets;
 
 //Construct fetch call
 submit.on('click', function() {
+    //Stop if no query input
+    searchError.empty();
+    if(query.val() === "") {
+        searchError.append("Bitte gib mindestens einen Suchbegriff ein.");
+        return;
+    }
+
+    var url = '/search?';
+    var parameters = [];
+
     //Check if query input
-    if(query.val().length !== 0) {
-        var q = query.val();
-        parameters.push(`search=${q}`);
-    }
-    //Check if plz input
-    if(plz.val().length !== 0) {
-        var q = plz.val();
-        parameters.push(`plz=${q}`);
-    }
+    var q = query.val();
+    parameters.push(`search=${q}`);
+
     //Check if filters chosen
-    if(doctors.is(":checked")) parameters.push(`fdoctors=true`);
     if(pharmacy.is(":checked")) parameters.push(`fpharmacy=true`);
     if(hospital.is(":checked")) parameters.push(`fhospital=true`);
-    if(redCross.is(":checked")) parameters.push(`fredCross=true`);
 
+    //Create fetch URL
     url = url + parameters.join('&');
-    console.log(url);
-    
+
+    //Fetch requested documents
+    fetchResults(url);
+})
+
+function handleClickSuggestions(e) {
+    var url = '/search?';
+    var parameters = [];
+
+    console.log(e.target);
+    parameters.push(`search=${q}`);
+
+    //Check if filters chosen
+    if(pharmacy.is(":checked")) parameters.push(`fpharmacy=true`);
+    if(hospital.is(":checked")) parameters.push(`fhospital=true`);
+
+    //Create fetch URL
+    url = url + parameters.join('&');
+
+    //Fetch requested documents
+    fetchResults(url);
+}
+
+function fetchResults(url) {
     fetch(url)
     .then(response => response.json())
     .then(response => {
-        console.log(response);
-    });
+        results = response.response.docs;
+        snippets = response.highlighting;
 
-    url = '/search?';
-})
+        if(response.response.numFound === 0) {
+            var suggestions = response.spellcheck.suggestions[1].suggestion;
+            var suggestionWords = [];
+
+            for(var i = 0; i < suggestions.length; i++) {
+                suggestionWords.push(`<span class="suggestion">"${suggestions[i].word}"</span>`);
+            }
+        }
+        displayResults(results, snippets, suggestionWords);
+    });
+}
+
+function displayResults(results, snippets, suggestions = []) {
+    var wrapper = $(".wrapper-result");
+    wrapper.empty();
+
+    if(results.length === 0) {
+        wrapper.append(`<p class="search-no-results">Kein Ergebnisse für \"${query.val()}\" gefunden.</p>
+        <p class="search-suggestions">Meintest du ${suggestions.join(" oder ")}? </p>`);
+    } else {
+        wrapper.append(`<p class="search-info">Suchergebnisse für: ${query.val()}</p>`);
+        for(var i = 0; i < results.length; i++) { 
+            //Get the highlighting for this particular result/ID
+            var snippet = snippets[results[i].id];
+            //Chain all snippets
+            var snippetString = Object.values(snippet).join(", ");
+
+            //Show all results with Bezeichnung/Adresse/Snippets
+            wrapper.append(`<div class="search-result">
+            <p class="result-header">${results[i].bezeichn_text_de}<p>
+            <p class="result-address">${results[i].str_txt_sort.join(" ")}, ${results[i].ort_text_de}</p>
+            <p class="result-snippets">${snippetString}</p>
+            </div>`);
+        };
+    }
+}
