4aecba03e1c168961d189f9443d010dbd9d4733e
Marion Menzl <mmenzl.mmt-b2016@fh-salzburg.ac.at>
2018-06-14 11:46:42 +0200

    Added query processing

--- Suchmaschine/index.js
+++ Suchmaschine/index.js
@@ -7,16 +7,36 @@ app.use(express.static('public'));
 const client = solr.createClient({
     host: 'localhost',
     port: 8983,
-    core: 'techproducts',
+    core: 'gettingstarted',
     path: '/solr'
 });
-const query = client.createQuery();
 
+//Craft query with request params, send query, return result
 app.get('/search', (req, res) => {
-    query.q({ 'text' : req.query.search });
-    client.search(query, function(err, obj){
+    const query = client.createQuery();
+
+    //Apply different weights to different fields
+    query
+    .dismax()
+    .q({"*" : req.query.search })
+	.qf({bezeichn_text_de : 4 , art_text_de : 3, ort_text_de: 3, str_txt_sort: 2})
+
+    //If both filters are chosen, nothing will be filtered
+    if(req.query.fpharmacy && req.query.fhospital);
+    //Filtered for pharmacies
+    else if(req.query.fpharmacy) query.matchFilter("art_text_de", "Apotheke");
+    //When filtered for hospitals, everything but "Apotheke" in art_text_de is requested
+    else if(req.query.fhospital) query.matchFilter("-art_text_de", "Apotheke");
+
+    //Returning Snippets
+    query.hl({
+        on: true,
+        fl: ["bezeichn_text_de", "art_text_de", "ort_text_de", "str_txt_sort"] 
+    });
+
+    client.spell(query, function(err, obj){
         if(err) console.log(err);
-        else res.send(obj.response.docs);
+        else res.send(obj);
     });
 });
 
